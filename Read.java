b7b673560b09c4477996d925de074d284f1a4880
VincentH-APCS <vhuang10@stuy.edu>
2021-01-04 13:18:21 -0500

    Forgot to commit this, basic test to make sure scanner copy exact text

---
+++ Read.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Read{
+  public static void main(String[] args){
+    Scanner n = new Scanner( System.in );
+    while(n.hasNext()){
+      String word = n.next();
+      System.out.print(word + " ");
+    }
+  }
+}
