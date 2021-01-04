86816a9eb26eeaf9a6e6de110697ffdaee5ee5fc
VincentH-APCS <vhuang10@stuy.edu>
2021-01-04 13:06:57 -0500

    Wrote the first attempt at the code of MakeStars, to be tested

---
+++ MakeStars.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class MakeStars{
+  public static void main(String[] args){
+    Scanner n = new Scanner(System.in);
+    while(in.hasNextLine()){
+      String line = in.NextLine();
+      Scanner p = new Scanner(line);
+      while(line.hasNext()){
+        System.out.println("*");
+      }
+      System.out.println();
+    }
+  }
+}
