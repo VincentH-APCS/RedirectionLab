7f430c9a3c357f94538da7a5373bfad25b4cad74
VincentH-APCS <vhuang10@stuy.edu>
2021-01-04 20:45:46 -0500

    Some simple tests to see if pigLatinSimple works as intended, to be tested with redirection now

---
+++ Demo.java
@@ -0,0 +1,12 @@
+public class Demo{
+  public static void main(String[] args){
+    System.out.println(PigLatin.pigLatinSimple("mock"));
+    System.out.println(PigLatin.pigLatinSimple("Mock"));
+    System.out.println(PigLatin.pigLatinSimple("MOCK"));
+    System.out.println(PigLatin.pigLatinSimple("pie"));
+    System.out.println(PigLatin.pigLatinSimple("david"));
+    System.out.println(PigLatin.pigLatinSimple("aaron"));
+    System.out.println(PigLatin.pigLatinSimple("aaroN"));
+    System.out.println(PigLatin.pigLatinSimple("elephant"));
+  }
+}
