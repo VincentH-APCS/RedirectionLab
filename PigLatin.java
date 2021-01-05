f6d4e24ad95b5b3ffc37fcf28b4f5bcd64d097c3
VincentH-APCS <vhuang10@stuy.edu>
2021-01-04 20:39:02 -0500

    Created the PigLatin Class and added the code for the Simple form of PigLatin, to be tested

---
+++ PigLatin.java
@@ -0,0 +1,16 @@
+public class PigLatin{
+  public static String pigLatinSimple(String s){
+    String output = s.toLowerCase();
+    String first = output.substring(0,1);
+    if(first.equals("a") || first.equals("e") || first.equals("i") ||
+    first.equals("o") || first.equals("u")) {
+      output = output + "hay";
+    }
+    else {
+      output = output.substring(1, output.length() - 1) + first + "ay";
+    }
+    s = output;
+    return output;
+    }
+
+  }
