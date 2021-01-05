75552d3a88a2a6fc061a8c3f47da12c1aa841421
VincentH-APCS <vhuang10@stuy.edu>
2021-01-04 20:54:55 -0500

    Wrote the code for the second method of PigLatin, to be tested for any issues

--- PigLatin.java
+++ PigLatin.java
@@ -13,4 +13,24 @@ public class PigLatin{
     return output;
     }
 
+    public static String pigLatin(String s){
+      String[] digraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr",
+       "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl",
+       "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+       String output = s.toLowerCase();
+       String first2 = output.substring(0,2);
+       int valid = 0;
+       for(int i = 0; i < digraphs.length; i++){
+         if(digraphs[i].equals(first2)){
+           valid = valid + 1;
+         }
+       }
+       if(valid == 1){
+         output = output.substring(2, output.length()) + first2 + "ay";
+         s = output;
+         return output;
+       }
+       else return pigLatinSimple(s);
+    }
+
   }
