05138387a9ee76117d37bcaa97d429c20977e7df
VincentH-APCS <vhuang10@stuy.edu>
2021-01-04 20:58:54 -0500

    Tested the second method of PigLatin, seems to run smoothly

--- Demo.java
+++ Demo.java
@@ -1,12 +1,22 @@
 public class Demo{
   public static void main(String[] args){
-    System.out.println(PigLatin.pigLatinSimple("mock"));
-    System.out.println(PigLatin.pigLatinSimple("Mock"));
-    System.out.println(PigLatin.pigLatinSimple("MOCK"));
-    System.out.println(PigLatin.pigLatinSimple("pie"));
-    System.out.println(PigLatin.pigLatinSimple("david"));
-    System.out.println(PigLatin.pigLatinSimple("aaron"));
-    System.out.println(PigLatin.pigLatinSimple("aaroN"));
-    System.out.println(PigLatin.pigLatinSimple("elephant"));
+    System.out.println(PigLatin.pigLatin("mock"));
+    System.out.println(PigLatin.pigLatin("Mock"));
+    System.out.println(PigLatin.pigLatin("MOCK"));
+    System.out.println(PigLatin.pigLatin("pie"));
+    System.out.println(PigLatin.pigLatin("david"));
+    System.out.println(PigLatin.pigLatin("aaron"));
+    System.out.println(PigLatin.pigLatin("aaroN"));
+    System.out.println(PigLatin.pigLatin("elephant"));
+    System.out.println(PigLatin.pigLatin("the"));
+    System.out.println(PigLatin.pigLatin("Check"));
+    System.out.println(PigLatin.pigLatin("skee"));
+    System.out.println(PigLatin.pigLatin("wrONG"));
+    System.out.println(PigLatin.pigLatin("emu"));
+    System.out.println(PigLatin.pigLatin("EMU"));
+    System.out.println(PigLatin.pigLatin("grade"));
+    System.out.println(PigLatin.pigLatin("aaron"));
+    System.out.println(PigLatin.pigLatin("plplpl"));
+    System.out.println(PigLatin.pigLatin("bruhhhhhh"));
   }
 }
