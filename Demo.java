a4f2fef3d49635d5f4ca687abeb6fdc55f9be887
VincentH-APCS <vhuang10@stuy.edu>
2021-01-04 21:35:24 -0500

    Tested PigLatinBest, now to redirect and test with file and sonnet

--- Demo.java
+++ Demo.java
@@ -1,22 +1,33 @@
 public class Demo{
   public static void main(String[] args){
-    System.out.println(PigLatin.pigLatin("mock"));
-    System.out.println(PigLatin.pigLatin("Mock"));
-    System.out.println(PigLatin.pigLatin("MOCK"));
-    System.out.println(PigLatin.pigLatin("pie"));
-    System.out.println(PigLatin.pigLatin("david"));
-    System.out.println(PigLatin.pigLatin("aaron"));
-    System.out.println(PigLatin.pigLatin("aaroN"));
-    System.out.println(PigLatin.pigLatin("elephant"));
-    System.out.println(PigLatin.pigLatin("the"));
-    System.out.println(PigLatin.pigLatin("Check"));
-    System.out.println(PigLatin.pigLatin("skee"));
-    System.out.println(PigLatin.pigLatin("wrONG"));
-    System.out.println(PigLatin.pigLatin("emu"));
-    System.out.println(PigLatin.pigLatin("EMU"));
-    System.out.println(PigLatin.pigLatin("grade"));
-    System.out.println(PigLatin.pigLatin("aaron"));
-    System.out.println(PigLatin.pigLatin("plplpl"));
-    System.out.println(PigLatin.pigLatin("bruhhhhhh"));
+    /*System.out.println(PigLatin.pigLatinBest("mock"));
+    System.out.println(PigLatin.pigLatinBest("Mock"));
+    System.out.println(PigLatin.pigLatinBest("MOCK"));
+    System.out.println(PigLatin.pigLatinBest("pie"));
+    System.out.println(PigLatin.pigLatinBest("david"));
+    System.out.println(PigLatin.pigLatinBest("aaron"));
+    System.out.println(PigLatin.pigLatinBest("aaroN"));
+    System.out.println(PigLatin.pigLatinBest("elephant"));
+    System.out.println(PigLatin.pigLatinBest("the"));
+    System.out.println(PigLatin.pigLatinBest("Check"));
+    System.out.println(PigLatin.pigLatinBest("skee"));
+    System.out.println(PigLatin.pigLatinBest("wrONG"));
+    System.out.println(PigLatin.pigLatinBest("emu"));
+    System.out.println(PigLatin.pigLatinBest("EMU"));
+    System.out.println(PigLatin.pigLatinBest("grade"));
+    System.out.println(PigLatin.pigLatinBest("aaron"));
+    System.out.println(PigLatin.pigLatinBest("plplpl"));
+    System.out.println(PigLatin.pigLatinBest("bruhhhhhh")); */
+    System.out.println(PigLatin.pigLatinBest("*emu"));
+    System.out.println(PigLatin.pigLatinBest("4chan"));
+    System.out.println(PigLatin.pigLatinBest("fish!"));
+    System.out.println(PigLatin.pigLatinBest("fish"));
+    System.out.println(PigLatin.pigLatinBest("the."));
+    System.out.println(PigLatin.pigLatinBest("THe."));
+    System.out.println(PigLatin.pigLatinBest("cat!"));
+    System.out.println(PigLatin.pigLatinBest("amazing?"));
+    System.out.println(PigLatin.pigLatinBest("aMAzing???"));
+    System.out.println(PigLatin.pigLatinBest("apple%"));
+
   }
 }
